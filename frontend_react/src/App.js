af23f877bdb307cee660317590d08988cf81f861
Nnamdi Okeke <onnamdialex@gmail.com>
2022-09-23 18:41:54 +0100

    :art: fmt/str still wip...

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+import About from "./container/About";
+import Footer from "./container/Footer";
+import Header from "./container/Header";
+import Skills from "./container/Skills";
+import Testimonials from "./container/Testimonial";
+import Work from "./container/Work";
+
 const App = () => {
   return (
     <div>
---
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Testimonials.scss";
+
+const Testimonial = () => {
+  return (
+    <div>
+      <h1>Testimonials</h1>
+    </div>
+  );
+};
+
+export default Testimonial;
