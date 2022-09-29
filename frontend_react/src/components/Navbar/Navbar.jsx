3dc35d5ed32eb5c2360b0f72c5c1fc03f8a4e053
Nnamdi Okeke <onnamdialex@gmail.com>
2022-09-29 20:06:15 +0100

    :construction: navbar wip...

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -5,10 +5,18 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <img src={images.no} alt="logo" />
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.no} alt="logo" height={40} />
       </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
