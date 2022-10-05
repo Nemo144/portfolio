728ef77a50f7f70c443ef31c39517dbbe2034399
Nnamdi Okeke <onnamdialex@gmail.com>
2022-10-05 17:24:16 +0100

    :construction: wip ...

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,11 +1,41 @@
 import React from "react";
-
+import { motion } from "framer-motion";
+import { images } from "../../constants";
 import "./Header.scss";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 1.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Nnamdi</h1>
+            </div>
+          </div>
+
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Freelancer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 1.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile" />
+      </motion.div>
+
+      <motion.div></motion.div>
     </div>
   );
 };
