decdbe3a2a450893f9f5c4312e557247cd372523
Nnamdi Okeke <onnamdialex@gmail.com>
2022-10-13 22:05:44 +0100

    :construction: about section wip ...

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 import "./About.scss";
 
+const abouts = [
+  {
+    title: "Web Development",
+    description: "I am a good web developer",
+    imgUrl: "",
+  },
+  // {
+  //   title: "Web Development",
+  //   description: "I am a good web developer",
+  //   imgUrl: "",
+  // },
+  // {
+  //   title: "Web Development",
+  //   description: "I am a good web developer",
+  //   imgUrl: "",
+  // },
+];
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <h2 className="head-text">
+        I know that
+        <span>Good Design</span>
+        <br />
+        means
+        <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div whileInView={{}}></motion.div>
+        ))}
+      </div>
+    </>
   );
 };
 
