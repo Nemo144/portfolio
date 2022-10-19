4c2ae9a592808b513aebae2b1b0e2d0d295151a4
Nnamdi Okeke <onnamdialex@gmail.com>
2022-10-19 14:05:57 +0100

    :rocket: About section done ...
    About section done. To be modified later to suit purposes!

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import { images } from "../../constants";
 
 import "./About.scss";
 
@@ -7,17 +8,22 @@ const abouts = [
   {
     title: "Web Development",
     description: "I am a good web developer",
-    imgUrl: "",
+    imgUrl: images.about01,
+  },
+  {
+    title: "Web Design",
+    description: "I am a good web designer",
+    imgUrl: images.about02,
   },
   // {
   //   title: "Web Development",
   //   description: "I am a good web developer",
-  //   imgUrl: "",
+  //   imgUrl: images.about03,
   // },
   // {
   //   title: "Web Development",
   //   description: "I am a good web developer",
-  //   imgUrl: "",
+  //   imgUrl: images.about04,
   // },
 ];
 
@@ -25,16 +31,27 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I know that
-        <span>Good Design</span>
-        <br />
-        means
-        <span>Good Business</span>
+        I know that <span>Good Web Development</span> <br /> means{" "}
+        <span>Good business</span>
       </h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
-          <motion.div whileInView={{}}></motion.div>
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
         ))}
       </div>
     </>
