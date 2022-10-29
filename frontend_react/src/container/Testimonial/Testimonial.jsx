0e735bb530fb6e9ed1a8b1e4f32eaa45612b269d
Nnamdi Okeke <onnamdialex@gmail.com>
2022-10-29 12:54:36 +0100

    :construction: testimonial section wip ...

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { HiChevronRight, HiChevronLeft } from "react-icons/hi";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
 import "./Testimonial.scss";
 
 const Testimonial = () => {
+  const [brands, setBrands] = useState([]);
   return (
     <div>
       <h1>Testimonials</h1>
