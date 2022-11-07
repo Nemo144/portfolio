ecba1e0c403d8b80a6bb94f0967c2c23549bb257
Nnamdi Okeke <onnamdialex@gmail.com>
2022-11-07 19:21:07 +0100

    :zap: contact section functionality done!

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -6,10 +6,108 @@ import { client } from "../../client";
 import "./Footer.scss";
 
 const Footer = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { name, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    //forming a contact object...
+    //in accordance with sanity guidelines
+    const contact = {
+      _type: "contact",
+      name: name,
+      email: email,
+      message: message,
+    };
+
+    //using the sanity client to upload data from my application
+    client.create(contact).then(() => {
+      //to get some info...
+      //whether loading was a success or not
+      //if loading was successful, we have
+      setLoading(false);
+
+      //if data was submitted successfully
+      setIsFormSubmitted(true);
+    });
+  };
+
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <>
+      <h2 className="head-text">Take a coffe & chat with me</h2>
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:onnamdialex@gmail.com" className="p-text">
+            onnamdialex@gmail.com
+          </a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel: +2347048101323" className="p-text">
+            +2347048101323
+          </a>
+        </div>
+      </div>
+
+      {!isFormSubmitted ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="text"
+              placeholder="Your Name"
+              onChange={handleChangeInput}
+              value={name}
+              name="name"
+            />
+          </div>
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="email"
+              placeholder="Your Email"
+              onChange={handleChangeInput}
+              value={email}
+              name="email"
+            />
+          </div>
+
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={message}
+              name={message}
+              onChange={handleChangeInput}
+            ></textarea>
+          </div>
+
+          <button type="button" className="p-text" onClick={handleSubmit}>
+            {loading ? "Sending" : "Sent Message"}
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3 className="head-text">Thank you for getting in touch</h3>
+        </div>
+      )}
+    </>
   );
 };
 
