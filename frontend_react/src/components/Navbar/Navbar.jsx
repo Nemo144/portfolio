462458c06dd47b16e669ed8e28a2ae1eb0ac1d45
Nnamdi Okeke <onnamdialex@gmail.com>
2022-11-11 16:38:29 +0100

    :coffin: remove dead code

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import { images } from "../../constants";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
 
@@ -9,9 +8,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <nav className="app__navbar">
-      <div className="app__navbar-logo">
-        {/* <img src={images.no} alt="logo" height={40} /> */}
-      </div>
+      <div className="app__navbar-logo"></div>
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
